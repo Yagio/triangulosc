0933f936a39a34a3d17c515d314171dae31a140a
Yagio <zacyarid@gmail.com>
2014-03-23 23:34:06 -0600

    Cambio en los valores de las variables de los ciclos para evitar imprimir la ultima linea en blanco

--- triangulo4.c
+++ triangulo4.c
@@ -14,11 +14,11 @@ int main(){
   printf("Altura del triangulo: ");
   scanf ("%d",&altura);
 
-  for (int i = 0; i <= altura; i++){
-    for (int y = 1; y <= i; y++){
+  for (int i = 1; i <= altura; i++){
+    for (int y = 1; y < i; y++){
       printf(" ");
     }
-    for (int x = i; x < altura; x++){
+    for (int x = i; x <= altura; x++){
       printf("*");
     }
     printf("\n");
